9bfd4d1dd8a29e374bf70adfc882d20e346667af
Emna HNANA <emnahnana21@gmail.com>
2021-01-23 00:12:49 +0100

    Sqlite

--- DataManagement/Data.py
+++ DataManagement/Data.py
@@ -1,1 +1,33 @@
-print("Hello World")
+import sqlite3
+
+# Connect to Database customer.db (does not already exist)
+
+conn = sqlite3.connect('customer.db')
+
+# ------------------ Create a Database Table -----------------
+
+# Create a cursor
+c = conn.cursor()
+
+# Create a Table
+c.execute('''CREATE TABLE customers1 (
+            first_name text,
+            last_name text,
+            email text)''')
+
+# Data types: NULL INTEGER REAL TEXT BLOB
+
+# Commit our command
+conn.commit()
+
+
+# ------------------ INSERT ONE RECORD IN A TABLE ---------
+
+c.execute('''INSERT INTO customers1 VALUES('Emna', 'HNANA', 'emna@gmail.com')
+            ''')
+print(" Command executed successfully ")
+
+conn.commit()
+
+# Close our connection
+conn.close()
