4cb40b9890375b7f9c787f56b219ba8ed43c25f8
Emna HNANA <emnahnana21@gmail.com>
2021-01-28 14:55:21 +0100

    rename file

---
+++ DataManagement/SQLite.py
@@ -0,0 +1,57 @@
+import sqlite3
+
+# Connect to Database customer.db (does not already exist)
+
+conn = sqlite3.connect('customer.db')
+
+# ------------------ Create a Database Table -----------------
+
+# Create a cursor
+c = conn.cursor()
+
+# Create a Table
+# c.execute('''CREATE TABLE customers4 (
+#             first_name text,
+#             last_name text,
+#             email text)''')
+
+# Data types: NULL INTEGER REAL TEXT BLOB
+
+# Commit our command
+conn.commit()
+
+
+# ------------------ INSERT ONE RECORD IN A TABLE ---------
+
+c.execute('''INSERT INTO customers VALUES('Emna', 'HNANA', 'emna@gmail.com')
+            ''')
+print(" Command executed successfully ")
+
+# ------------------ INSERT MANY RECORDS----------------------
+many_customers = [('Wes', 'Brown', 'wes@brown.com'), ('Steph', 'Lola', 'steph@lola.com')]
+
+c.executemany('''INSERT INTO customers VALUES 
+                    (?, ?, ?)''', many_customers)
+
+conn.commit()
+
+# ------------------- Fetch and Query -------------------------
+c.execute("SELECT * FROM customers")
+items = c.fetchall()
+
+# print an item in one line
+for item in items:
+    print(item)
+
+# print only first element in an item
+for item in items:
+    print(item[0])
+
+# ---------------- Primary key -----------------
+    
+
+
+# ------------------- Close connection -------------------------
+
+# Close our connection
+conn.close()
